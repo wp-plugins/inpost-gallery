0c138d865525b1ea5e0af66bca04f3431880af1e
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-24 17:04:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/inpost-gallery/trunk@535641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/model.php
@@ -0,0 +1,61 @@
+<?php
+
+class INPOSTGALLERY_Model {
+
+    const TABLE_GALLERY = "inpostgallery";
+
+    private $db;
+
+    public function __construct() {
+        global $wpdb;
+        $this->db = $wpdb;
+    }
+
+    public function get_post_data($post_id) {
+        $data = $this->db->get_results("SELECT * FROM " . self::TABLE_GALLERY . " WHERE post_id=$post_id", ARRAY_A);
+        return array("gallery_data" => $data, "post_id" => $post_id);
+    }
+
+    //for shortcode only
+    public function get_post_data_by_ids($ids) {
+        $ids = explode(",", $ids);
+        $ids_set = "(";
+        if (!empty($ids)) {
+            foreach ($ids as $key => $image) {
+                if ($key > 0) {
+                    $ids_set.=",";
+                }
+                $ids_set.=$image['id'];
+            }
+        }
+        $ids_set.=")";
+
+        $data = $this->db->get_results("SELECT * FROM " . self::TABLE_GALLERY . " WHERE id IN" . $ids_set, ARRAY_A);
+        return array("gallery_data" => $data);
+    }
+
+    public function save_image($post_id, $new_image_name, $image_name) {
+        $this->db->insert(self::TABLE_GALLERY, array('post_id' => $post_id, 'image_file_name' => $new_image_name, 'image_title' => $image_name));
+    }
+
+    public function edit_image($image_id, $title) {
+        $this->db->update(self::TABLE_GALLERY, array('image_title' => $title), array('id' => $image_id));
+    }
+
+    public function delete_image($image_id) {
+        $image_info = $this->get_image_info($image_id);
+        print_r($image_info);
+        @unlink(INPOST_GALLERY_PLUGIN_PATH . "uploads/" . $image_info['post_id'] . "/" . $image_info['image_file_name']);
+        @unlink(INPOST_GALLERY_PLUGIN_PATH . "uploads/" . $image_info['post_id'] . "/thumbs/" . $image_info['image_file_name']);
+
+        $this->db->query("DELETE FROM " . self::TABLE_GALLERY . " WHERE `id` =" . $image_id);
+    }
+
+    //***
+    private function get_image_info($image_id) {
+        $data = $this->db->get_results("SELECT * FROM " . self::TABLE_GALLERY . " WHERE id=$image_id", ARRAY_A);
+        return $data[0];
+    }
+
+}
+
