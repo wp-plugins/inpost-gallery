1c5659720d38ae7f12c26beaa62bec8e991fcc75
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-24 17:07:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/inpost-gallery/trunk@535647 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/front_js.php
@@ -0,0 +1,7 @@
+
+<script type="text/javascript" src="<?= INPOST_GALLERY_PLUGIN_LINK ?>js/yoxview/yoxview-init.js"></script>
+<script type="text/javascript">
+    LoadScript("<?= INPOST_GALLERY_PLUGIN_LINK ?>js/yoxview/yoxview-nojquery.js");
+    var inpostgallery_ajax_url="<?= INPOST_GALLERY_PLUGIN_LINK ?>index.php";
+</script>
+
