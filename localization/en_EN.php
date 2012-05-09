2d943295c15a3b0eb1116062b07562c72413c6fa
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-09 18:26:18 +0000

    git-svn-id: https://plugins.svn.wordpress.org/inpost-gallery/trunk@541980 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ localization/en_EN.php
@@ -0,0 +1,13 @@
+<?php
+
+$inpostgallery_vocabulary = array(
+    "settings" => "Settings",
+    "delete" => "Delete",
+    "edit" => "Edit",
+    "update" => "Update",
+    "cancel" => "Cancel",
+    "save" => "Save",
+    "chooce_files" => "Chooce files",
+    "sure" => "Sure?",
+);
+
