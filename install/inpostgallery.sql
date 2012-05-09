4ba868dac2edd9da09f8ae8a2eec66d69e160e03
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-09 18:21:50 +0000

    git-svn-id: https://plugins.svn.wordpress.org/inpost-gallery/trunk@541972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- install/inpostgallery.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `inpostgallery` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `post_id` int(11) NOT NULL,
-  `image_file_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `image_title` text COLLATE utf8_unicode_ci NOT NULL,
-  `image_description` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
