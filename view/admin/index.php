42815f7f27cdf060298a42f6abe83ba9ec9e8054
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-01 21:59:39 +0000

    git-svn-id: https://plugins.svn.wordpress.org/inpost-gallery/trunk@552045 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ view/admin/index.php
@@ -0,0 +1,6 @@
+<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+<h2>InPost Gallery</h2>
+
+Made on http://www.yoxigen.com/yoxview/ gallery javascript. For future in settings will be opportunity to change javascript gallery for another frameworks.<br />
+Please set ftp permissions for folder "uploads" in plugin directory as 0777, do it before plugin using. You can show images you select as above in shortcode. 
+For example if you use mealingua plugin, on each translation is possible to show it's own gallery.<br />
---
+++ view/front/shortcode.php
@@ -0,0 +1,21 @@
+<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+
+
+<?php if (!empty($gallery_data)): ?>
+    <div class="yoxview">
+        <?php foreach ($gallery_data as $image) : ?>
+
+            <a href="<?= INPOST_GALLERY_PLUGIN_LINK . "uploads/" . $image['post_id'] . "/" . $image['image_file_name'] ?>"><img src="<?= INPOST_GALLERY_PLUGIN_LINK . "uploads/" . $image['post_id'] . "/thumbs/" . $image['image_file_name'] ?>" alt="<?= $image['image_title'] ?>" title="<?= $image['image_title'] ?>" /></a>
+
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
+
+<script type="text/javascript">
+
+    ﻿jQuery(document).ready(function($){
+       jQuery.noConflict();
+       jQuery(".yoxview").yoxview();
+    });
+</script>
+
